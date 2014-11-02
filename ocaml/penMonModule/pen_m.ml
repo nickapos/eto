5086d0ce2ea54893cd76bba246a723765542f7be
Nick Apostolakis <nickapos@oncrete.gr>
2014-11-02 11:59:16 +0000

    changing makefile back to normal, after failed experiment in static compiling. more method ported in penmon class

--- ocaml/penMonModule/pen_m.ml
+++ ocaml/penMonModule/pen_m.ml
@@ -118,13 +118,100 @@ class penm = object (self)
      *)
     let angle=(2.*.BatFloat.pi/.365.)*.j-.1.39 in
       0.409*.sin(angle)
-  (*
-  method sun_hour_angle J L=
+
+  method sun_hour_angle j l=
+    (*
+     *function that calculates the
+      sunset hour angle from input the
+      latitude in rad and the solar declination
+
+      returns the sunset hour angle ws
+     *)
+      acos(-.tan(self#lat_in_rad l)*.tan(self#solar_declination j))
+
+
   method daylength ws=
-  method clear_short_radiation J L=
-  method stef_boltz_temp_prod T=
-  method calculate =
-*)
+    (*
+     *function that calculates the daylength
+      by using the  sunset hour angle ws
+
+      returns daylength
+     *)
+     24.*.ws/.BatFloat.pi
+  method clear_short_radiation j l=
+    (*
+     function that calculates the
+    radiation from the sun taking under
+    consideration the angle of the
+    position of the field and the
+    leng of the day.
+    gsc is the solar constant
+
+    returns Ra
+   *)
+   let gsc=0.0820 and 
+    phi=self#lat_in_rad l and 
+    delta=self#solar_declination j and 
+    ws=self#sun_hour_angle j l and 
+    dr=self#inv_rel_dist j in
+   let expression=24.*.60.*.gsc*.dr*.(ws*.sin phi *.sin(delta)+.cos(phi)*.cos(delta)*.sin(ws)) in
+    expression/.BatFloat.pi
+
+  method stef_boltz_temp_prod t=
+    (*
+     *a function that calculates
+      the product of the stefan boltzman
+      with the temperature
+
+      returns the product 
+     *)
+     let sigma=4.903*.10.**(-9.) and 
+        tk=t+.273.16 in
+      sigma*.tk**4.
+
+  method calculate tmax tmin altitude u=
+    (*
+     *function used to calculate the ETo
+
+      returns ETo
+     *)
+    let tmean= (tmax +. tmin)/. 2. and press=self#pressure altitude in
+    let g=self#gamma press and temp1=1.+.0.34*.u and delt=self#delta tmean in
+      delt/.(delt+.g+.temp1)
+   (* 
+   delt=self.delta(Tmean)
+   temp2=delt/(delt+G*temp1)
+   temp3=G/(delt+G*temp1)
+   temp4=900*self.U/(Tmean+273)
+   emax=self.e_svp(self.Tmax)
+   emin=self.e_svp(self.Tmin)
+   eav=(emax+emin)/2
+   deltaEs = eav-self.Ea
+   J=self.day_of_year(self.Day,self.Month)
+   L=self.Latitude_Moires+(float(self.Latitude_Lepta)/60)
+   Ra=self.clear_short_radiation(J,L)
+   ws=self.sun_hour_angle(J,L)
+   N=self.daylength(ws)
+   nN=self.n/N
+   Rs=(0.25+0.5*nN)*Ra
+   Rso=(0.75+2*self.altitude/100000)*Ra
+   logos1=Rs/Rso
+   Rns=0.77*Rs
+   sigma_t_max = self.stef_boltz_temp_prod(self.Tmax)
+   sigma_t_min=self.stef_boltz_temp_prod(self.Tmin)
+   temp5= 0.34-0.14*math.sqrt(self.Ea)
+   temp6=1.35*logos1-0.35
+   Rnl=((sigma_t_max+sigma_t_min)/2)*temp5*temp6
+   Rn=Rns-Rnl
+   Gday=0
+   Gmonth=0.14*(self.Tmonth_i-self.Tmonth_i_1)
+   temp7=Rn-Gmonth
+   temp8 = 0.408*(Rn-G)
+   fin1 = temp8 * temp2
+   fin2 = temp4*deltaEs*temp3
+   ETo=fin1+fin2
+   return ETo *)
+
 
 end;;
 
