7e3317f278df16682cf222e5d575295569a5888f
Nick Apostolakis <nickapos@oncrete.gr>
2014-11-24 18:12:58 +0000

    adding tests in clean target and refactoring test main

--- ocaml/tests/unit.ml
+++ ocaml/tests/unit.ml
@@ -1,12 +1,15 @@
 open OUnit2
+
+
+
+let test1 test_ctxt = assert_equal ~msg:"failed" ~printer:string_of_int 1 1
+let test2 test_ctxt = assert_equal ~msg:"failed" ~printer:string_of_int 2 1
+
 let _ =
 begin
-  let suite = "mytest">::
-  (fun test_ctxt ->
-   assert_equal 
-     ~msg:"failed" 
-     ~printer:string_of_int
-     1 1
-  ) in
+  let suite = "mytest">:::
+    ["test1" >:: test1;
+     "test2">:: test2 
+    ] in
   run_test_tt_main suite
 end
