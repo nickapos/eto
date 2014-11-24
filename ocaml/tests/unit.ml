f0f7abfee0488470354a11f5909108b556ec473d
Nick Apostolakis <nickapos@oncrete.gr>
2014-11-24 09:59:15 +0000

    first working dummy test with ounit2

--- ocaml/tests/unit.ml
+++ ocaml/tests/unit.ml
@@ -1,37 +1,12 @@
-open Should
-
-let int_test_case () =
-    let x = 123 in begin
-        x $hould # equal 123;
-        x $hould # not # equal 0;
-
-        x $hould # be # above 122;
-        x $hould # be # at # most 124;
-
-        x $hould # be # within (122,123);
-        x $houldn't # be # within (1,3)
-    end
-
-
-let list_test_case () =
-    let x = [1; 2; 3] in begin
-        list x $houldn't # be # empty;
-        list x $hould # have # length 3;
-        list x $hould # contain 2;
-        list x $hould # not # contain 0
-    end
-
-let fun_test_case () =
-    let f = (function "" -> invalid_arg "f" | s -> String.lowercase s) in begin
-        calling f "" $hould # raise # any # exn;
-        calling f "" $hould # raise # exn # prefixed "Invalid_argument";
-        calling f "foo" $houldn't # raise # any # exn
-    end
-
-let lala () = Printf.printf "%s\n" "lala"
-
-let lala_test ()=
-  1 $hould # equal 1
-
-let _= lala()
-
+open OUnit2
+let _ =
+begin
+  let suite = "mytest">::
+  (fun test_ctxt ->
+   assert_equal 
+     ~msg:"failed" 
+     ~printer:string_of_int
+     1 1
+  ) in
+  run_test_tt_main suite
+end
