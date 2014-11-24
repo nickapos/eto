5625f47b599fe207939e7a320d66db3421403046
Nick Apostolakis <nickapos@oncrete.gr>
2014-11-24 21:31:38 +0000

    first functional tests for ea_base

--- ocaml/tests/ea_base_test.ml
+++ ocaml/tests/ea_base_test.ml
@@ -1,13 +1,26 @@
 open OUnit2
+open EaBase
 
+let tmin=15.0
+let tmax=24.6
+let obj = new eaBase
 
+let e_test test_ctxt=
+  let should_be=2.39921378082 in
+  let compare =cmp_float ~epsilon:0.01 (obj#e tmax) should_be and 
+    error_msg="failed test result: "^string_of_float (obj#e tmax)^" should be "^string_of_float should_be in
+    assert_bool error_msg compare
 
-let test2 test_ctxt=
-  let a =cmp_float ~epsilon:0.00001 0.1999999 0.1999998 in
-    assert_bool "failed" a
+let calc_es_test test_ctxt=
+  let should_be=1.7053462321157722 in
+  let compare =cmp_float ~epsilon:0.01 (obj#calc_es tmin tmax) 1.7053462321157722 and 
+    error_msg="failed test result: "^string_of_float (obj#calc_es tmin tmax)^" should be "^string_of_float should_be in
+    assert_bool error_msg compare
 
-
-let suite2 = "suite2">::: ["test2" >:: test2; ]
+let eabase = "eabase">::: [
+    "e_test" >:: e_test; 
+    "calc_es_test" >:: calc_es_test; 
+  ]
 
 let _ =
-  run_test_tt_main suite2
+  run_test_tt_main eabase
