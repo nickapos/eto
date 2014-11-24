0ec36bf3eb152dace6ccbaee46055f871b157308
Nick Apostolakis <nickapos@oncrete.gr>
2014-11-24 20:19:51 +0000

    adding cmp_float test

--- ocaml/tests/unit.ml
+++ ocaml/tests/unit.ml
@@ -3,13 +3,13 @@ open OUnit2
 
 
 let test1 test_ctxt = assert_equal ~msg:"failed" ~printer:string_of_int 1 1
-let test2 test_ctxt = assert_equal ~msg:"failed" ~printer:string_of_int 2 1
+let test2 test_ctxt=
+  let a =cmp_float ~epsilon:0.1 10. 9. in
+    assert_bool "failed" a
+
+
+let suite1 = "suite1">::: ["test1" >:: test1;"test2" >:: test2; ]
+let suite2 = "suite2">::: ["test2" >:: test2; ]
 
 let _ =
-begin
-  let suite = "mytest">:::
-    ["test1" >:: test1;
-     "test2">:: test2 
-    ] in
-  run_test_tt_main suite
-end
+  run_test_tt_main suite1
