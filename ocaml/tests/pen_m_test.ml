17b0622ec612015aad8dcb8860cd2b78f6d64700
Nick Apostolakis <nickapos@oncrete.gr>
2014-11-29 11:30:11 +0000

    starting adding tests in pen_m class

--- ocaml/tests/pen_m_test.ml
+++ ocaml/tests/pen_m_test.ml
@@ -1,7 +1,9 @@
 open OUnit2
 open Pen_m
 
-let test_precision=0.09
+let obj = new penm
+
+let test_precision=0.01
 let tMin = 25.6
 let tMax=34.8
 let ea=2.85
@@ -14,17 +16,34 @@ let monthlyAvPrev=29.2
 let alt=2.0
 let sun=8.5
 
-let obj = new penm
 
+let pressure_test_val=100.
 
+let pressure_test _=
+  let should_be=100.123508 and result=obj#pressure pressure_test_val in
+  let compare =cmp_float ~epsilon:test_precision result should_be and 
+  error_msg="failed test result: "^string_of_float (result)^" should be "^string_of_float should_be^" diff is: "^string_of_float (result-.should_be) in
+    assert_bool error_msg compare
+
+let gamma_p_test_val=100.
+
+let gamma_test _=
+  let should_be=0.0665 and result=obj#gamma gamma_p_test_val in
+  let compare =cmp_float ~epsilon:test_precision result should_be and 
+  error_msg="failed test result: "^string_of_float (result)^" should be "^string_of_float should_be^" diff is: "^string_of_float (result-.should_be) in
+    assert_bool error_msg compare
+(*
 let eto_fin_test _=
   let should_be=5.72 and result=obj#calculate ~tmin:tMin ~tmax:tMax ~ea:ea ~day:15 ~avairspeed:2. ~monthNum:5 ~latitude_degrees:latDeg ~latitude_Lepta:latMin ~tmonth_i:monthlyAv ~tmonth_i_1:monthlyAvPrev ~altitude:alt ~av_sunhours:sun in
   let compare =cmp_float ~epsilon:test_precision result should_be and 
   error_msg="failed test result: "^string_of_float (result)^" should be "^string_of_float should_be^" diff is: "^string_of_float (result-.should_be) in
     assert_bool error_msg compare
 
-
-let eto_test = "eto_test">::: ["eto_fin_test" >:: eto_fin_test; ]
+*)
+let eto_test = "eto_test">::: [
+  "pressure_test">:: pressure_test;
+  "gamma_test">:: gamma_test;
+   ]
 
 let _ =
   run_test_tt_main eto_test
