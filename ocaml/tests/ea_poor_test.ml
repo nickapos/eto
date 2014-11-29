0bcee29d5cb1ebe9b50b4e0aafc8acad6b24e454
Nick Apostolakis <nickapos@oncrete.gr>
2014-11-29 01:41:32 +0000

    externalizing test precision from the main tests

--- ocaml/tests/ea_poor_test.ml
+++ ocaml/tests/ea_poor_test.ml
@@ -1,6 +1,7 @@
 open OUnit2
 open EaPoor
 
+let test_precision=0.01
 let tmin=18.0
 let tmax=25.0
 let rhmean=0.68
@@ -9,12 +10,12 @@ let obj = new eaPoor
 
 let test2 _=
   let should_be=1.78 and result=(obj#calc_ea rhmean tmin tmax)*.100.  in
-  let compare =cmp_float ~epsilon:0.01 result  should_be and 
+  let compare =cmp_float ~epsilon:test_precision result  should_be and 
   error_msg="failed test result: "^string_of_float (result)^" should be "^string_of_float should_be in
     assert_bool error_msg compare
 
 
-let suite2 = "suite2">::: ["test2" >:: test2; ]
+let eapoor_test = "eapoor_test">::: ["test2" >:: test2; ]
 
 let _ =
-  run_test_tt_main suite2
+  run_test_tt_main eapoor_test
