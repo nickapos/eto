b26c7fed6290c87ca9dde70ff06e136732c3cd77
Nick Apostolakis <nickapos@oncrete.gr>
2014-11-30 11:36:47 +0000

    adding more tests

--- ocaml/tests/pen_m_test.ml
+++ ocaml/tests/pen_m_test.ml
@@ -32,6 +32,25 @@ let gamma_test _=
   let compare =cmp_float ~epsilon:test_precision result should_be and 
   error_msg="failed test result: "^string_of_float (result)^" should be "^string_of_float should_be^" diff is: "^string_of_float (result-.should_be) in
     assert_bool error_msg compare
+
+let e_svp_test _=
+  let should_be=3.28277117 and result=obj#e_svp tMin in
+  let compare =cmp_float ~epsilon:test_precision result should_be and 
+  error_msg="failed test result: "^string_of_float (result)^" should be "^string_of_float should_be^" diff is: "^string_of_float (result-.should_be) in
+    assert_bool error_msg compare
+
+let is_leap_y_test _=
+  (* test a leap year *)
+  let year=2012 in
+  let result=obj#is_leap year and error_msg="failed test result year "^string_of_int (year)^" is not leap " in
+    assert_bool error_msg result
+
+let is_leap_f_test _=
+  (* test a non leap year*)
+  let year=2010 in
+  let result=obj#is_leap year and error_msg="failed test result year "^string_of_int (year)^" is leap " in
+  let negated_result= not result in
+    assert_bool error_msg negated_result
 (*
 let eto_fin_test _=
   let should_be=5.72 and result=obj#calculate ~tmin:tMin ~tmax:tMax ~ea:ea ~day:15 ~avairspeed:2. ~monthNum:5 ~latitude_degrees:latDeg ~latitude_Lepta:latMin ~tmonth_i:monthlyAv ~tmonth_i_1:monthlyAvPrev ~altitude:alt ~av_sunhours:sun in
@@ -43,6 +62,9 @@ let eto_fin_test _=
 let eto_test = "eto_test">::: [
   "pressure_test">:: pressure_test;
   "gamma_test">:: gamma_test;
+  "e_svp_test">:: e_svp_test;
+  "is_leap_y_test">:: is_leap_y_test;
+  "is_leap_f_test">:: is_leap_f_test;
    ]
 
 let _ =
